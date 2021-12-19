f1606d3d113682f1fddae960bd92d2198752f3e5
William <kravendahunter@hotmail.com>
2021-12-19 02:29:46 -0600

    modified code to match my needs

--- Quickstart.py
+++ Quickstart.py
@@ -1,17 +1,23 @@
 # Import from the file
 from weather import Weather
 
-your_api_key = ''
-city_name = ''
+city_name = input("What city's weather would you like? ")
+print("What unit of measure for the weather?")
+unit = input("> ")
 
 # Create an instance of weather object
-instance = Weather(your_api_key)
+instance = Weather(your_api_key, unit)
 
 # Make a dictionary filled with information about the city by this method
 data = instance.getWeather(city_name)
 
 # This is just an example of how you should be printing this
-temprature = f'Temprature: {round(data["temprature"])}°C'
+name = f'City Name: {data["name"]}'
+if unit == "metric":
+    notation = "C"
+else:
+    notation = "F"
+temperature = f'Temperature: {round(data["temperature"])}°{notation}'
 pressure = f'Pressure: {round(data["pressure"])} Pascal'
 humidity = f'Humidity: {data["humidity"]} g.m-3'
 weather_condition = f'Weather Condition: {data["description"]}'
@@ -19,4 +25,4 @@ wind_speed = f'Wind Speed: {data["wind_speed"]} m/s'
 wind_direction = f'Wind Direction: {data["wind_direction"]}°'
 
 
-print(f'{temprature} \n{pressure} \n{humidity} \n{weather_condition} \n{wind_speed} \n{wind_direction}')
+print(f'{name} \n{temperature} \n{pressure} \n{humidity} \n{weather_condition} \n{wind_speed} \n{wind_direction}')
